22761d54ae96fe911c61c8e6fb2775943cf54122
devteamua <dev@sicenter.ua>
2020-10-09 10:24:16 +0200

    Added "msgId" in the assertion ignore List for step "a002MQGETMessage"

--- demo.rockitizer/src/test/java/MQinMQOutTest.java
+++ demo.rockitizer/src/test/java/MQinMQOutTest.java
@@ -1,8 +1,8 @@
-
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.xmlunit.diff.ElementSelectors;
 
+import com.google.common.collect.ImmutableList;
 import com.rockit.common.blackboxtester.assertions.XMLFileAssertion;
 import com.rockit.common.blackboxtester.suite.structures.TestBuilder;
 import com.rockit.common.blackboxtester.wrapper.AbstractTestWrapper;
@@ -20,14 +20,14 @@ public class MQinMQOutTest extends AbstractTestWrapper {
 		// read messages from queue after waiting and error
 		testBuilder.addStep("a002MQGetError").execute();
 		testBuilder.addStep("a002MQGETMessage").execute();
-		
+
 		// write messages to inputQueue and wait
 		testBuilder.addStep("a003MQPutMessage").execute().sleep(5000);
 
 		// read messages from queue after waiting and error
 		testBuilder.addStep("a004MQGetError").execute();
 		testBuilder.addStep("a004MQGETMessage").execute();
-				
+
 		// write messages to inputQueue and wait
 		testBuilder.addStep("a005MQPutMessage").execute().sleep(5000);
 
@@ -35,18 +35,12 @@ public class MQinMQOutTest extends AbstractTestWrapper {
 		testBuilder.addStep("a006MQGetError").execute();
 		testBuilder.addStep("a006MQGETMessage").execute();
 
-	
-
 		// do assertion with recorded values if mode is reply
 		testBuilder.addAssertion(new XMLFileAssertion("a002MQGETMessage").withNodeMatcher(ElementSelectors.byNameAndText)
 		// .ignoreAttrs(ImmutableList.of("updateTimestamp",
 		// "insertTimestamp","processingTime"))
-		// .ignore(ImmutableList.of("updateTimestamp",
-		// "insertTimestamp","beginTimestamp", "endTimestamp",
-		// "addressDate","businessConnectionDate"))
-				.checkForSimilar());
-		
-	
+				.ignore(ImmutableList.of("msgId")).checkForSimilar());
+
 		// do assertion with recorded values if mode is reply
 		testBuilder.addAssertion(new XMLFileAssertion("a004MQGETMessage").withNodeMatcher(ElementSelectors.byNameAndText)
 		// .ignoreAttrs(ImmutableList.of("updateTimestamp",
@@ -55,8 +49,7 @@ public class MQinMQOutTest extends AbstractTestWrapper {
 		// "insertTimestamp","beginTimestamp", "endTimestamp",
 		// "addressDate","businessConnectionDate"))
 				.checkForSimilar());
-		
-	
+
 		// do assertion with recorded values if mode is reply
 		testBuilder.addAssertion(new XMLFileAssertion("a006MQGETMessage").withNodeMatcher(ElementSelectors.byNameAndText)
 		// .ignoreAttrs(ImmutableList.of("updateTimestamp",
