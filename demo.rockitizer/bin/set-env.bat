d671a2fbca572ac627202c9bf0e5d0e1190242c5
devteamua <dev@sicenter.ua>
2020-10-19 14:26:15 +0200

    CLI Dependency Resolution over Maven

--- demo.rockitizer/bin/set-env.bat
+++ demo.rockitizer/bin/set-env.bat
@@ -1,1 +1,2 @@
+set REPO=%userprofile%\.m2\repository
 set CLASSPATH_PREFIX=../target/test-classes;target/test-classes
--- demo.rockitizer/bin/set-env.sh
+++ demo.rockitizer/bin/set-env.sh
@@ -1,1 +1,2 @@
+set REPO=$HOME/.m2/repository
 export CLASSPATH_PREFIX=../target/test-classes;target/test-classes;
